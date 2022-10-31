8310c8a7dfb4e4a54979711675eab030aaa71b15
SWETALI MOHANTY <84954770+swetali33@users.noreply.github.com>
2022-10-31 17:49:29 +0530

    Add files via upload

---
+++ 01_C_PROG/02_Operators/Lab Session/exp4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+int a,b;
+printf("Enter the value of a : -");
+scanf("%d",&a);
+printf("Enter the value of b : -");
+scanf("%d",&b);
+printf("The value of A is :- %d and B is :- %d ",a,b);
+
+}
