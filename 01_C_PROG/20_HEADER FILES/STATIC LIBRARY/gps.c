c81faca696bf4517ced56117e2eafa817ab07be9
SWETALI MOHANTY <84954770+swetali33@users.noreply.github.com>
2022-11-01 09:44:06 +0530

    Add files via upload

---
+++ 01_C_PROG/20_HEADER FILES/STATIC LIBRARY/gps.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void gps();
+
+void gps()
+{
+    printf("\nWelcome to Gps Module!!\n");
+}
---
+++ 01_C_PROG/20_HEADER FILES/STATIC LIBRARY/lcd.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void lcd();
+
+void lcd()
+{
+    printf("\nWelcome to lcd Module!!\n");
+}
